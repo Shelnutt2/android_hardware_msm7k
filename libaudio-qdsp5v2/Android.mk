c0b2cd31f17782473eece7061eef1b64ec2fbafe
Shelnutt2 <Shelnutt2@gmail.com>
2011-11-22 19:25:13 -0500

    ported cm patches

--- libaudio-qdsp5v2/Android.mk
+++ libaudio-qdsp5v2/Android.mk
@@ -4,22 +4,23 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := libaudio
+LOCAL_MODULE := audio.primary.qdsp5v2
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_STATIC_LIBRARIES += libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES := libaudiohw_legacy
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libutils \
     libmedia \
     libhardware_legacy \
-    libsysutils
+    libsysutils \
+    libdl
 
 ifeq ($TARGET_OS)-$(TARGET_SIMULATOR),linux-true)
 LOCAL_LDLIBS += -ldl
 endif
 
-ifneq ($(TARGET_SIMULATOR),true)
-LOCAL_SHARED_LIBRARIES += libdl
-endif
 
 LOCAL_SRC_FILES += AudioHardware.cpp
 
@@ -36,17 +37,20 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-+LOCAL_SRC_FILES:=               \
-+    AudioPolicyManager.cpp
+LOCAL_SRC_FILES:=   AudioPolicyManager.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
     libutils \
     libmedia
 
-LOCAL_STATIC_LIBRARIES := libaudiopolicy_legacy
 
-LOCAL_MODULE:= libaudiopolicy
+
+LOCAL_MODULE:= audio_policy.qdsp5v2
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_STATIC_LIBRARIES := libmedia_helper
+LOCAL_WHOLE_STATIC_LIBRARIES := libaudiopolicy_legacy
+LOCAL_MODULE_TAGS := optional
 
 LOCAL_STATIC_LIBRARIES += libaudiointerface
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
